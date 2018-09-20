2e8e59c057d3206a89a8c142fe1ff039461f8ff1
chengyangwangmc3 <43442467+chengyangwangmc3@users.noreply.github.com>
2018-09-20 09:39:02 -0400

    Added line 8 for creation date

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -5,5 +5,6 @@ int main ()
 {
 	cout << "Computers, computers everywhere"; // display a string
 	cout << "\n as far as I can C"; // display another string
+	cout << "\n Created on 9/20/2018";
 	return 0;
 }
